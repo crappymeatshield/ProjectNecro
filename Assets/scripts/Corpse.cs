660f10be7615da1a573d2c5cb0fccd4aba554ec3
rufflezberg <erickson.russell.j@gmail.com>
2016-01-30 22:03:37 -0600

    Corpse class start and unnecessary debugs removed

---
+++ Assets/scripts/Corpse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corpse : MonoBehaviour {
+
+    public GameObject corpse;
+    public string lifeTag;
+
+	// Use this for initialization
+	void Start () {
+        corpse = GameObject.FindGameObjectWithTag("Corpse");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+}
