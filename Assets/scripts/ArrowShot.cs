73f5c3d7ac8d1c8d1a11f1924319b777bfa2bea9
Vyrkyll <cradkay@yahoo.com>
2016-01-31 01:44:47 -0600

    3rd try? Bigger Farm, skinned things

--- Assets/scripts/ArrowShot.cs
+++ Assets/scripts/ArrowShot.cs
@@ -13,17 +13,7 @@ public class ArrowShot : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        //enemy = GameObject.FindGameObjectWithTag("ShootingEnemy");
-        ////player = GameObject.FindGameObjectWithTag("Player");
-        //stats = enemy.GetComponent<Stats>();
         arrowRB = GetComponent<Rigidbody2D>();
-        //float x = player.transform.position.x - transform.position.x;
-        //float y = player.transform.position.y - transform.position.y;
-        //arrowPos = new Vector2(x, y);
-        //float degrees = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
-        //startPos = transform.position;
-        //transform.rotation = Quaternion.Euler(0, 0, degrees - 90);
-        //arrowPos.Normalize();
     }
 
     // Update is called once per frame
